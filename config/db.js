5934ada74e2339c8996c144235b52326c0811056
Valliyappan Ramasamy <valliyappanramasamy610@gmail.com>
2024-06-08 00:27:53 +0530

    added few more changes

--- config/db.js
+++ config/db.js
@@ -2,13 +2,26 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/.env" });
 
-const Connection = async () => {
-  try {
-    await mongoose.connect(process.env.URI);
-    console.log("Connected");
-  } catch (err) {
-    console.error("Error: " + err.message);
-  }
-};
+// const Connection = async () => {
+//   try {
+//     await mongoose.connect(process.env.URI);
+//     console.log("Connected");
+//   } catch (err) {
+//     console.error("Error: " + err.message);
+//   }
+// };
 
-Connection();
+// Connection();
+
+console.log("Database URL:", process.env.DATABASE_URL);
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import "./config/db.js";
 import { Router } from "./routes/routes.js";
-
-dotenv.config({ path: "./config/.env" });
+import "./config/db.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config({ path: "./config/.env" });
+
 app.use("/bestcrm", Router);
 
-app.listen(process.env.PORT, () => {
-  console.log("App is Running");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
