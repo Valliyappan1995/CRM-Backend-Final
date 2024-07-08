a203215e7d23c6c28598d4f589cd678b4c609f32
Valliyappan Ramasamy <valliyappanramasamy610@gmail.com>
2024-07-08 08:22:12 +0530

    changes done

--- config/db.js
+++ config/db.js
@@ -2,26 +2,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/.env" });
 
-const Connection = async () => {
-  try {
-    await mongoose.connect(process.env.URI);
-    console.log("Connected");
-  } catch (err) {
-    console.error("Error: " + err.message);
-  }
-};
-
-Connection();
-
-// console.log("Database URL:", process.env.DATABASE_URL);
-// mongoose
-//   .connect(process.env.DATABASE_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to MongoDB Atlas");
-//   })
-//   .catch((error) => {
-//     console.error("Error connecting to MongoDB Atlas:", error);
-//   });
+console.log("Database URL:", process.env.DATABASE_URL);
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
